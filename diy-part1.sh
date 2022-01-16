671f05d74de3fed3a08e2d026fc2f5511446e268
neverj1019 <neverj1019@gmail.com>
2022-01-16 15:23:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,9 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*kenzo\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*small\)/\1/' feeds.conf.default
+
 
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
